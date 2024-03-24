f9321d9acb380d908a3d46b02e5cc271f1f35f2f
annbisha <annskaylove@gmail.com>
2024-03-24 16:32:33 +0200

    Small landing page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
